f1d507f6904ea53d9e4cb39b23d385bd607cca05
Craig <suesscraig112715@gmail.com>
2021-06-01 19:02:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,10 @@
 
 def fizzbuzz(int)
   if int % 5 == 0 && int % 3 == 0
-    return "Fizz" + "Buzz"
-  else 
+    return "Fizz" + "Buzz";
+  elsif int % 5 == 0
+    return "Buzz";
+  else
     return nil; 
   end 
 end 
